b56849a0ca35e4973074098b73752890ce9bf84d
sahinalam13 <mohon20022@gmail.com>
2025-08-23 11:31:03 +0600

    add js file and some changes

---
+++ script.js
@@ -0,0 +1,19 @@
+// login button functionality
+document.getElementById("loginButton")
+.addEventListener('click', function(e){
+    e.preventDefault();
+    const mobileNumber = 15756843500;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById("mobile-number").value;
+    const mobileNumberValueConverter = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById("pin-number").value;
+    const pinNumberValueConverter = parseInt(pinNumberValue);
+
+    if(mobileNumberValueConverter === mobileNumber && pinNumberValueConverter === pinNumber){
+        window.location.href="./main.html"
+    }
+    else{
+        alert("Invalid Credentials");
+    }
+})
