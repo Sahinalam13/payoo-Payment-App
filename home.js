31e542462ca507c47d649bbdbdfff6cf959cc3fb
Sahinalam13 <mohon20022@gmail.com>
2025-08-23 17:02:56 +0600

    add home.js some changes

--- home.js
+++ home.js
@@ -1,1 +1,29 @@
-console.log("connected");
+const pinNumber = 1234;
+
+document.getElementById("add-money-btn").addEventListener('click', function(e){
+    e.preventDefault();
+   const bankValue = document.getElementById("bank").value;
+   const accountNumberValue = document.getElementById("accoun-number").value;
+   const addPinvalue = parseInt(document.getElementById("add-pin").value); 
+   const addAmountValue = parseInt(document.getElementById("add-amount").value);
+   const availableBalanceValue = parseInt(document.getElementById("available-balance").innerText);
+
+   if(accountNumberValue.length < 11){
+    alert("please provide a valid account number");
+    return;
+   }
+
+   if(addPinvalue !== pinNumber){
+    alert("please provide a valid pin");
+    return;
+   }
+
+
+
+   const totalAvailableBalance = addAmountValue + availableBalanceValue;
+   document.getElementById("available-balance").innerText = totalAvailableBalance;
+  
+
+
+
+})
